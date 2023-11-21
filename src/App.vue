9a242eba5c4f80ab42e6efcb9c9c0048acc53019
RedJone888 <redjoan.guo@gmail.com>
2023-11-21 11:39:33 +0800

    提交husky格式化后的代码

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box">
+    <h1>我是App</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+for(let i=0;i<100;i++)
+{
+  console.log(i);
+  
+}
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
